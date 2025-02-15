8bb63389f617693521e7e68893d31cbbf6822682
geraldtm <99144289+GeraldTM@users.noreply.github.com.>
2025-02-15 12:41:06 -0500

    better request handleing

--- server.py
+++ server.py
@@ -1,4 +1,4 @@
-import socket, json
+import socket, json, os
 
 class TCPServer:
     def __init__(self, host='127.0.0.1', port=8888 ):
@@ -40,12 +40,43 @@ class HTTPServer(TCPServer):
     
     def handle_request(self, data):
         print(data)
-        response_line = self.response_line(200)
-        headers = self.response_headers()
+
+        request = HTTPRequest(data)
+
+        try:
+            handler = getattr(self, 'handle_%s' % request.method)
+        except AttributeError:
+            handler = self.HTTP_501_handler
+        response = handler(request)
+        print(response)
+        return response
+
+    def HTTP_501_handler(self, request):
+        response_line = self.response_line(status_code=501)
+        response_headers = self.response_headers()
         blank_line = b"\r\n"
-        response_body = open("./test.html", "rb").read()
-        return b"".join([response_line, blank_line, response_body])
-    
+        response_body = b"<h1> 501 Not Implemented </h1> <br> <p> This site has not implemented a response to "+request.method.encode()+b" requests </p>"
+
+        return b"".join([response_line, response_headers, blank_line, response_body])
+
+    def handle_GET(self, request):
+        filename = request.uri.strip('/')
+
+        if os.path.exists(filename):
+            response_line = self.response_line(status_code=200)
+
+            response_headers = self.response_headers()
+
+            with open(filename, 'rb') as f:
+                response_body = f.read()
+        else:
+            response_line = self.response_line(status_code=404)
+            response_headers = self.response_headers()
+            response_body = b"<h1>404 Not Found <br> <p> file "+filename.encode()+ b" not found </p>"
+        blank_line = b"\r\n"
+        return b"".join([response_line, response_headers, blank_line, response_body])
+
+
     def response_line(self, status_code):
         reason = self.status_codes['%s' % status_code]
         line = 'HTTP/1.1 %s %s \r\n' % (status_code, reason['message'])
@@ -63,6 +94,28 @@ class HTTPServer(TCPServer):
             headers += '%s: %s\r\n' % (header, headers_copy[header])
         
         return headers.encode()
+
+class HTTPRequest:
+    def __init__(self, data):
+        self.method = None
+        self.uri = None
+        self.http_version = "1.1"
+
+        self.parse(data)
+
+    def parse(self, data):
+        lines = data.split(b"\r\n")
+        request_line = lines[0]
+
+        words = request_line.split(b" ")
+        self.method = words[0].decode()
+
+        if len(words) > 1:
+            self.uri = words[1].decode()
+
+        if len(words) > 2:
+            self.http_version = words[2]
+
 
 if __name__ == '__main__':
     server = HTTPServer()
